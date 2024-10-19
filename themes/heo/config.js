afa34fd8f395eccf0702477dbb53a4e127786f7e
小仔 <93524089+littleboy1322@users.noreply.github.com>
2024-10-19 11:05:56 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,8 +4,7 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
-
+  HEO_SITE_CREATE_TIME: '2024-10-19', // 建站日期，用于计算网站运行的第几
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '感謝使用小仔系統下載中心!', url: 'https://0012310.xyz' },
